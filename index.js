ba2ea8d21de96ceb84d2a72a99472b29fac9018c
Julio Ramos <julio@jcsr.com.br>
2023-02-16 18:48:38 -0300

    new api
    new api
    get books done
    get versicles done

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const axios = require('axios');
-const versions = require('./versions');
+const books = require('./books');
+const verses = require('./verses');
 
 
 
@@ -8,51 +9,160 @@ const versions = require('./versions');
 
     try {
         const resultado = await database.sync();
-        console.log(resultado);
+        //console.log(resultado);
     } catch (error) {
-        console.log(error);
+        //console.log(error);
     }
 })();
 
+/* newUser();
+async function newUser() {
 
-async function doPostRequest() {
+    var config = {
+        method: 'post',
+        url: `https://www.abibliadigital.com.br/api/users`,
+        name: "Julio",
+        email: "julioramos.esporte@gmail.com",
+        password: "250133",
+        notifications: true,
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        }
+    };
 
-    let res3 = await axios.get('https://api.scripture.api.bible/v1/bibles',
+    let resposta = await axios.post('https://www.abibliadigital.com.br/api/users', config);
+    console.log(resposta)
+} */
+
+
+async function getVerses() {
+
+    //buscando todos os livros da biblia no banco de dados
+    let b = await books.findAll();
+    let i = 0
+    let t = b.length
+    console.log(b)
+    //loop para os livros da biblia
+    while (i < t) {
+
+        //loop para os capitulos do livro
+        
+        let tc = parseInt(b[i].chapters) 
+        let ic = 1
+        console.log(typeof tc)
+
+        while (ic < tc) {
+            
+            let lb = b[i].abbrevPt
+
+            let res3 = await axios.get(`https://www.abibliadigital.com.br/api/verses/acf/${lb}/${ic}`,
+                {
+                    headers: {
+                        'Content-Type': 'application/json',
+                        'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IlRodSBGZWIgMTYgMjAyMyAxMzoyNjoyOCBHTVQrMDAwMC5qdWxpb3JhbW9zLmVzcG9ydGVAZ21haWwuY29tIiwiaWF0IjoxNjc2NTUzOTg4fQ.FRY5EHQxhmagki9e9EH4DoR4HUBRs0S8pEshjH10cq0'
+                    }
+                })
+
+
+
+            let l = res3.data
+            //loop dos versos do capitulo
+            let versos = l.verses
+            let iv = 0
+            let tv = versos.length
+
+            while (iv < tv) {
+                await verses.create({
+                    bookAbbrev: lb,
+                    chapterNumber: ic,
+                    totalVerses: l.chapter.verses,
+                    verseNumber: l.verses[iv].number,
+                    verse: l.verses[iv].text
+                })
+                iv++
+            }
+            //createVerse(l) 
+            //console.log(l)
+            ic++
+        }
+        i++
+    }
+
+    /* let res3 = await axios.get('https://www.abibliadigital.com.br/api/verses/acf/gn/2',
         {
             headers: {
-                'api-key': 'ad29b519974afee5a969822c9a6d5703'
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IlRodSBGZWIgMTYgMjAyMyAxMzoyNjoyOCBHTVQrMDAwMC5qdWxpb3JhbW9zLmVzcG9ydGVAZ21haWwuY29tIiwiaWF0IjoxNjc2NTUzOTg4fQ.FRY5EHQxhmagki9e9EH4DoR4HUBRs0S8pEshjH10cq0'
+            }
+        }) */
+    /* 
+        let l = res3.data
+        createVerse(l) */
+    //console.log(l)
+
+}
+
+
+async function getBooks() {
+
+    let res3 = await axios.get('https://www.abibliadigital.com.br/api/books',
+        {
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IlRodSBGZWIgMTYgMjAyMyAxMzoyNjoyOCBHTVQrMDAwMC5qdWxpb3JhbW9zLmVzcG9ydGVAZ21haWwuY29tIiwiaWF0IjoxNjc2NTUzOTg4fQ.FRY5EHQxhmagki9e9EH4DoR4HUBRs0S8pEshjH10cq0'
             }
         })
 
+    let l = res3.data
+    l.forEach(createBook)
+    //console.log(l)
 
-    let l = res3.data.data
-    l.forEach(createVersion)
-    console.log(l)
+}
+
+async function createBook(b) {
+
+    const createBook = await books.create({
+        abbrevPt: b.abbrev.pt,
+        abbrevEn: b.abbrev.en,
+        author: b.author,
+        chapters: b.chapters,
+        group: b.group,
+        name: b.name,
+        testament: b.testament,
+    })
+}
+
+async function createVerse(v) {
+
+    let versos = v.verses
+    let i = 0
+    let t = versos.length
+
+    console.log(t)
+    while (i < t) {
+        await verses.create({
+            bookAbbrev: 'gn',
+            chapterNumber: 2,
+            totalVerses: v.chapter.verses,
+            verseNumber: v.verses[i].number,
+            verse: v.verses[i].text
+        })
+        i++
+    }
+
 
 }
 
-doPostRequest()
+async function robots() {
 
-async function createVersion(l) {
-
-
-    const resultadoCreateVersion = await versions.create({
-        id: l.id,
-        dblId: l.dblId,
-        relatedDbl: l.relatedDbl,
-        name: l.name,
-        nameLocal: l.nameLocal,
-        abbreviation: l.abbreviation,
-        description: l.description,
-        descriptionLocal: l.descriptionLocal,
-        languageName: l.language.name,
-        languageNameLocal: l.language.nameLocal,
-        script: l.language.script,
-        scriptDirection: l.language.scriptDirection,
-        countryName: l.countries[0].name,
-        countryNameLocal: l.countries[0].nameLocal
-    }) 
-
-
+    //await getBooks()
+    await getVerses()
 
 }
+
+robots()
+
+
+
+
